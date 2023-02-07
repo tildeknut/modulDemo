c542d13b10ba337be7c74aac0c8d65f8fce00ec8
Hannah <hannahmk@uia.no>
2023-02-07 16:08:15 +0100

    Commiter innlevering

--- Dictionary.mjs
+++ Dictionary.mjs
@@ -1,39 +1,35 @@
 
+import eng from './test/lan/en.json' assert { type: 'json' };
+import no from './test/lan/no.json' assert { type: 'json' };
+
 class Dictionary{
-
     constructor(...languages){
-        this.languagefiles = languages
+        this.languagefiles = languages;
         this.dictionary = null;
+        
     }
-
-    setLanguage = function(){
-        // Velger språket som skal brukes.
-
-        // Laster språket fra riktig fil (se this.languages)
+    setLanguage = function(dictionary){
+        this.languagefiles = dictionary;
+        this.dictionary = dictionary;
+        
     }
-
     get = function(key){
         // Henter riktig verdi basert på key i dictionary.
+        console.log(this.dictionary[key])
         return this.dictionary[key];
     }
-
     keys = function(){
         // retunerer alle keys i dictionary 
         return Object.keys( this.dictionary);
     }
-
-    
-
 }
 
-DICTIONARY_KEYS = {
+let DICTIONARY_KEYS = {
     end:"end"
 }
 
-let dictionary = new Dictionary("./lan/no.json");
-dictionary.setLanguage("no");
+let dictionary = new Dictionary('./test/lan/');
+dictionary.setLanguage(eng);
 dictionary.get(DICTIONARY_KEYS.end);
-
-
 
 export default Dictionary;
